ba93460f60a782ff15ee9b2b0eb81bc1777a67d5
Harshil <200456136@student.georgianc.on.ca>
2021-02-02 01:06:25 -0500

    Updated group Assignment 1

--- Group Assignment 1/Program.cs
+++ Group Assignment 1/Program.cs
@@ -33,8 +33,27 @@ namespace In_Class_Assignment_1
             int busTrips = Convert.ToInt32(Console.ReadLine());
 
 
-            //Calculate total and average cost of trips
+            //Calculate total cost of trips
             int totalCost = (planeTrips * 200) + (trainTrips * 80) + (busTrips * 50);
+
+            //If the user enters 0 trips, display error message and keep asking him to enter a valid number of trips
+            while (totalCost == 0)
+            {
+                Console.WriteLine("");
+                Console.Write("You must enter at least one travel.");
+                Console.Write("Please select how many times you which to tavel.");
+                Console.WriteLine("");
+                Console.Write("How many times do you wish to travel by Plane?");
+                planeTrips = Convert.ToInt32(Console.ReadLine());
+                Console.Write("How many times do you wish to travel by Train?");
+                trainTrips = Convert.ToInt32(Console.ReadLine());
+                Console.Write("How many times do you wish to travel by Bus?");
+                busTrips = Convert.ToInt32(Console.ReadLine());
+
+                totalCost = (planeTrips * 200) + (trainTrips * 80) + (busTrips * 50);
+            }
+
+            //Calculate average cost of trips
             int averageCost = totalCost / (planeTrips + trainTrips + busTrips);
 
             //Print costs by using the replacement code
@@ -44,56 +63,68 @@ namespace In_Class_Assignment_1
            
 
             //Question 2
-            //The minimum eligibility to crack a course
+            //The minimum eligibility to pass a course
             Console.WriteLine("");
             Console.WriteLine("Question 2");
             Console.WriteLine("");
             Console.WriteLine("The minimum eligibility to passed the course, based upon the following criteria...");
             Console.WriteLine("");
 
-            //Printing the minimum requirements in criteria A)
+            //Print the minimum requirements in criteria A)
             Console.WriteLine("A). The minimum passing marks should be: ");
             Console.WriteLine("Marks in CSharp >=60");
             Console.WriteLine("Marks in Java >=55");
             Console.WriteLine("Marks in Python >=50");
             Console.WriteLine("");
 
-            //Printing the minimum requirements in criteria B)
+            //Print the minimum requirements in criteria B)
             Console.WriteLine("B). Another condition to pass the course: ");
             Console.WriteLine("Total in all three subjects >=170");
             Console.WriteLine("OR");
             Console.WriteLine("Total in CSharp and Python >=150");
             Console.WriteLine("");
 
-            //Asking the user to input the marks for the subjects.
+            //Ask the user to input the marks for the subjects.
             Console.WriteLine("Now enter the marks you obtained in the above criteria: ");
             Console.WriteLine("");
             Console.Write("Marks in CSharp: ");
-            int inCSharp = Convert.ToInt32(Console.ReadLine());
+            int cSharpGrade = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Marks in Java: ");
-            int inJava = Convert.ToInt32(Console.ReadLine());
+            int javaGrade = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Marks in Python: ");
-            int inPython = Convert.ToInt32(Console.ReadLine());
+            int pythonGrade = Convert.ToInt32(Console.ReadLine());
 
-            //Calculating the total marks
-            int totalmarks = inCSharp + inJava + inPython;
-            int marks = inCSharp + inPython;
-            Console.WriteLine("");
-            Console.WriteLine("The total marks in all 3 subjects: {0}", totalmarks);
-            Console.WriteLine("The marks in Csharp and Pyhton are: {0}", marks);
+            //Calculate the total marks
+            int threeClassGrade = cSharpGrade + javaGrade + pythonGrade;
+            int twoClassGrade = cSharpGrade + pythonGrade;
 
-            //Instantianing ternary operator to check whether the above eligibility criteria is met or not 
-            //if yes, then eligible. if not, then not eligible.
-            string finalResult = ((inCSharp >= 60 && inJava >= 55 && inPython >= 50) || 
-                                 (inCSharp + inJava + inPython >= 170) || 
-                                 (inCSharp + inPython >= 150)) ? 
-                                 "Eligible for admission" : 
-                                 "not eligible for admission";
-
-            Console.WriteLine("The student is {0}", finalResult);
-
+            //Evaluate if student can be admitted
+            if (cSharpGrade >= 60 && javaGrade >= 55 && pythonGrade >= 50)
+            {
+                if (threeClassGrade >= 170 || twoClassGrade >= 150)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("The candidate is eligible for admission");
+                    Console.WriteLine("CSharp Grade: {0}, Java Grade: {1}, Python Grade: {2}", cSharpGrade, javaGrade, pythonGrade);
+                    Console.WriteLine("Grade for the three classes: {0}, Grade for CSharp and Python: {1}", threeClassGrade, twoClassGrade);
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("The candidate is NOT eligible for admission");
+                    Console.WriteLine("CSharp Grade: {0}, Java Grade: {1}, Python Grade: {2}", cSharpGrade, javaGrade, pythonGrade);
+                    Console.WriteLine("Grade for the three classes: {0}, Grade for CSharp and Python: {1}", threeClassGrade, twoClassGrade);
+                }
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("The candidate is NOT eligible for admission");
+                Console.WriteLine("CSharp Grade:{0}, Java Grade: {1}, Python Grade: {2}", cSharpGrade, javaGrade, pythonGrade);
+                Console.WriteLine("Grade for the three classes: {0}, Grade for CSharp and Python: {1}", threeClassGrade, twoClassGrade);
+            }
         }
     }
 }
